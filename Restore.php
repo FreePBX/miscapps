9187735c7d118325a463c757873b4d75682c77e9
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 17:45:08 -0500

    [Module Tag script: miscapps 15.0.3] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -2,24 +2,13 @@
 namespace FreePBX\modules\Miscapps;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = reset($this->getConfigs());
-    foreach ($configs as $destination) {
-        $this->FreePBX->Miscapps->upsert($destination['miscapps_id'], $destination['description'], $destination['ext'], $destination['dest'], $destination['enabled']);
-    }
-  }
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $tables = array_flip($tables+$unknownTables);
-    if(!isset($tables['miscapps'])){
-      return $this;
-    }
-    $bmo = $this->FreePBX->Miscapps;
-    $bmo->setDatabase($pdo);
-    $data = $bmo->listApps(true);
-    $bmo->resetDatabase();
-    foreach ($data as $destination) {
-      $bmo->upsert($destination['miscapps_id'], $destination['description'], $destination['ext'], $destination['dest'], $destination['enabled']);
-    }
-    return $this;
-  }
+	public function runRestore($jobid){
+		$configs = reset($this->getConfigs());
+		foreach ($configs as $destination) {
+				$this->FreePBX->Miscapps->upsert($destination['miscapps_id'], $destination['description'], $destination['ext'], $destination['dest'], $destination['enabled']);
+		}
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		$this->restoreLegacyDatabase($pdo);
+	}
 }
