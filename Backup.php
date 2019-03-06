dd1359bb6e05b35d10a20ffe4934360e9c533313
Andrew Nagy <andrew.nagy@the159.com>
2019-03-06 17:35:42 -0500

    [Module Tag script: miscapps 15.0.4]

--- Backup.php
+++ Backup.php
@@ -2,8 +2,11 @@
 namespace FreePBX\modules\Miscapps;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-  public function runBackup($id,$transaction){
-    $configs = $this->FreePBX->Miscapps->listApps(true);
-    $this->addConfigs($configs);
-  }
+	public function runBackup($id,$transaction){
+		$configs = $this->FreePBX->Miscapps->listApps(true);
+		$this->addConfigs([
+			'data' => $configs,
+			'features' => $this->dumpFeatureCodes()
+		]);
+	}
 }
--- Restore.php
+++ Restore.php
@@ -3,10 +3,11 @@ namespace FreePBX\modules\Miscapps;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
 	public function runRestore($jobid){
-		$configs = reset($this->getConfigs());
-		foreach ($configs as $destination) {
-				$this->FreePBX->Miscapps->upsert($destination['miscapps_id'], $destination['description'], $destination['ext'], $destination['dest'], $destination['enabled']);
+		$configs = $this->getConfigs();
+		foreach ($configs['data'] as $destination) {
+			$this->FreePBX->Miscapps->upsert($destination['miscapps_id'], $destination['description'], $destination['ext'], $destination['dest'], $destination['enabled']);
 		}
+		$this->importFeatureCodes($config['features']);
 	}
 	public function processLegacy($pdo, $data, $tables, $unknownTables){
 		$this->restoreLegacyDatabase($pdo);
